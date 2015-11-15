e7283b1e755e41b4cb553a701c103ad63b3f5f91
LukeSkypewalker <work37@gmail.com>
2015-11-15 21:18:29 +0300

    +Songs

--- client/app.js
+++ client/app.js
@@ -1,6 +1,9 @@
 Template.index.helpers({
-	songNames: function() {
-		return Songs.find({songName: {$exists: true}}, {name: 1, _id: 0});
+	songList: function() {
+		//TODO: Sort List ( I've tried, but i didn't successed)
+		// return Songs.find({songName: {$exists: true}}, {artist: 1, songName: 1, _id: 1}, {sort: {artist: 1}} );
+		// return Songs.find({songName: {$exists: true}}, {artist: 1, songName: 1, _id: 1} ).fetch().sort({artist: 1});
+		return Songs.find({songName: {$exists: true}}, {artist: 1, songName: 1, _id: 1});
 	},
 	songs: function() {
 		var nxtSong = Songs.findOne({_id: 'currentSong'}, {_id:0, currentSongName: 1} );
